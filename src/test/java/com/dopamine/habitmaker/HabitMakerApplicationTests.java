e6ea2e6adf2685a1d302320e3a4f2c84ee7f75b5
MinsooKang <minsu1599@naver.com>
2026-05-03 17:25:46 +0900

    초기 spring boot 4.0.6 설정

---
+++ src/test/java/com/dopamine/habitmaker/HabitMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dopamine.habitmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
